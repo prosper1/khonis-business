ce5d25ee43bad3db826fd0ce9918b6ca4f41bbb9
gundotshili@gmail.com <gundotshili@gmail.com>
2020-09-14 05:39:34 -1100

    added final user flow

--- src/app/ambulance/map/map.page.ts
+++ src/app/ambulance/map/map.page.ts
@@ -1,3 +1,4 @@
+import { AppComponent } from './../../app.component';
 import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 
@@ -8,13 +9,36 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
+  latitude = -25.7590793;
+  longitude = 28.2068974;
 
-  constructor() { }
+  center = {
+    latitude: -25.7590793,
+    longitude: 28.2068974,
+  };
+  constructor( public geolocation: Geolocation) { }
 
   ngOnInit() {
   }
 
   // get location and save it to storage
   // Update latitude and longitude for map center and user position(marker)
+  getLocation(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+
+    const watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      });
+  }
+
+
+
+
+
 
 }
